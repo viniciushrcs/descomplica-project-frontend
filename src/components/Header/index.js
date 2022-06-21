a8f55da6dce44854a157051810d9d0bbc7e6f7bd
ViniciusChagas <viniciushrcs@gmail.com>
2022-06-21 03:01:50 -0300

    refactor: changes header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,15 +2,17 @@ import { HeaderWrapper, HeaderContent, Logo } from "./styles";
 
 export function Header() {
   return (
-    <HeaderWrapper>
-      <HeaderContent>
-        <Logo>
-          <img
-            src="https://d3awytnmmfk53d.cloudfront.net/landings/static/images/core/logo_verde.svg"
-            alt="logo-descomplica"
-          />
-        </Logo>
-      </HeaderContent>
-    </HeaderWrapper>
+    <>
+      <HeaderWrapper>
+        <HeaderContent>
+          <Logo>
+            <img
+              src="https://d3awytnmmfk53d.cloudfront.net/landings/static/images/core/logo_verde.svg"
+              alt="logo-descomplica"
+            />
+          </Logo>
+        </HeaderContent>
+      </HeaderWrapper>
+    </>
   );
 }
