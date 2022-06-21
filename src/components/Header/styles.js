304139fbcb899e74a5dbccc6504d500163ba9c31
ViniciusChagas <viniciushrcs@gmail.com>
2022-06-20 23:27:26 -0300

    feat: add Header component

---
+++ src/components/Header/styles.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const HeaderWrapper = styled.div`
+  background: white;
+  width: 100%;
+`;
+
+export const HeaderContent = styled.div`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  padding: 10px 20px;
+  justify-content: flex-start;
+`;
+
+export const Logo = styled.span`
+  width: 127px;
+  height: 37px;
+`;
